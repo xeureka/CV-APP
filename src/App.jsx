0a662f2a78bc4aace3bb687ce6f631e44580654d
xeureka <teklemariameureka@gmail.com>
2024-12-02 10:07:24 +0300

    general information section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Form from './components/Form'
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Form />
     </div>
   )
 }
