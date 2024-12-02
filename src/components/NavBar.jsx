1cdb583526acb9aae12ab2e68ebbe2b14fa5e5d5
xeureka <teklemariameureka@gmail.com>
2024-12-02 13:23:00 +0300

    typewriter n the navbar added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Typewriter } from 'react-simple-typewriter'
 
 const NavBar = () => {
   return (
-    <div className='bg-slate-800 text-white font-bold font-mono text-5xl p-4'>
-      CV Builder
+    <div className='flex justify-center bg-slate-800 text-white font-bold font-mono text-5xl p-4'>
+      <Typewriter 
+        words={['CV BUILDER']}
+        loop={5}
+        cursor
+        cursorStyle='|'
+        typeSpeed={100}
+        deleteSpeed={80}
+        delaySpeed={100}
+        
+      />
+
     </div>
   )
 }
