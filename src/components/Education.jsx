55756e38a363d61efde3a7afbb665a9352c98377
xeureka <teklemariameureka@gmail.com>
2024-12-02 10:27:45 +0300

    Education boiler plate Done

---
+++ src/components/Education.jsx
@@ -0,0 +1,25 @@
+import React,{useState} from 'react'
+
+const Education = () => {
+    const [count,setCount] = useState(0)
+  return (
+    <div>
+      <h1 className='my-6 font-bold text-3xl'>Education #{count}</h1>
+
+      <div className='flex flex-col gap-12'>
+        <input type="text" placeholder='Degree' className='p-4 rounded-lg'/>
+        <input type="text" placeholder='Univeristy or School Name' className='p-4 rounded-lg'/>
+        <input type="text" placeholder='Course' className='p-4 rounded-lg'/>
+        <input type="text" placeholder='To' className='p-4 rounded-lg'/>
+      </div>
+
+      <div className='flex gap-12 justify-end mt-6'>
+        <button className='text-xl px-8 py-4 font-mono text-white font-bold bg-slate-900 rounded-lg'>Add</button>
+        <button className='text-xl px-8 py-4 font-mono text-white font-bold bg-slate-900 rounded-lg'>Delete</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Education
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,11 @@
 import React,{useState} from 'react'
 import General from './General'
-
+import Education from './Education'
 const Form = () => {
   return (
     <div className='m-16 border-2 p-4 border-black rounded-lg w-2/5 '>
       <General />
+      <Education />
     </div>
   )
 }
