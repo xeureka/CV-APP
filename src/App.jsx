5a42aa3b1978acc7aee38361900f71b1eb1a76f0
xeureka <teklemariameureka@gmail.com>
2024-12-02 14:48:19 +0300

    the display section completed and

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Form from './components/Form'
+import Display from './components/Display'
 
 const App = () => {
   return (
-    <div>
+    <div className='h-screen'>
+
       <NavBar />
-      <Form />
+
+      <div className='flex flex-row gap-4'>
+        <Form />
+
+        <Display />
+      </div>
+     
     </div>
   )
 }
---
+++ src/components/Display.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Profile from '../assets/pfp2.jpeg'
+import {FaMobile,FaEnvelope,FaHouseUser} from 'react-icons/fa'
+
+const Display = () => {
+  return (
+    <div className='m-16 border-2  border-black rounded-lg w-2/5 flex '>
+      <div className='h-full bg-slate-600 w-2/5'>
+            <div className='pt-24 pl-12'>
+              <img src={Profile} alt="Sample Profile image" className='rounded-full '/>
+            </div>
+
+
+            <div className='text-white font-bold text-4xl underline flex flex-col items-center h-1/2 w-full mt-36'>
+              <h1>Contacts</h1>
+             <div className='items mt-4'>
+              <p ><span className='mt-8 text-2xl'><FaMobile/></span></p>
+              <p className='mt-8 text-2xl'><span className='text-2xl'><FaEnvelope /></span></p>
+              <p className='mt-8 text-2xl'><span><FaHouseUser /></span></p>
+             </div>
+
+             <div>
+              <h1 className='text-white font-bold text-4xl mt-24 underline'>Education</h1>
+             </div>
+            </div> 
+        </div>
+
+      <div className='ml-8 flex flex-col '>
+        <h1 className='font-bold text-6xl mt-48'>Eureka Teklemariam Haile</h1>
+        <div>
+          <h1 className='font-bold font-mono underline text-3xl mt-24'>Profile</h1>
+        </div>
+
+        <div>
+          <h1 className='font-bold text-3xl underline font-mono mt-24'>Word Experience</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Display
